79d192b2813dc500523f42c58615fbc493d6404f
yangyu <yangyu2010@aliyun.com>
2020-03-14 14:21:56 +0800

    222

--- amplifier/ListViewController.m
+++ amplifier/ListViewController.m
@@ -11,6 +11,9 @@
 #import <UIColor+Utils.h>
 #import <MBProgressHUD/MBProgressHUD.h>
 #import <Toast.h>
+#import <MJRefresh/MJRefresh.h>
+#import "MJRefreshAutoNormalFooter.h"
+#import "MJRefreshNormalHeader.h"
 
 #import "ListTableViewCell.h"
 
@@ -18,6 +21,10 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *arrDatas;
+
+
+@property (nonatomic, assign) NSInteger page;
+
 
 @end
 
@@ -30,27 +37,77 @@
     self.tableView.delegate = self;
     [self.tableView registerNib:[UINib nibWithNibName:@"ListTableViewCell" bundle:nil] forCellReuseIdentifier:@"ListTableViewCellID"];
     
-    self.arrDatas = [NSMutableArray array];
+    self.tableView.mj_footer = [MJRefreshAutoNormalFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreData)];
+//    [self.tableView.mj_footer beginRefreshing];
     
+    self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadData)];
+
+    self.arrDatas = [NSMutableArray array];
+    [self loadData];
+}
+
+- (void)loadData {
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
         
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     
-    [manager GET:@"http://94.191.30.13/notification/list?pageNum=1&pageSize=10" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    _page = 1;
+    
+    NSString *url = [NSString stringWithFormat:@"http://94.191.30.13/notification/list?pageSize=10&pageNum=%ld", (long)_page];
+    [manager GET:url parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         [MBProgressHUD hideHUDForView:self.view animated:YES];
         if ([responseObject[@"code"] integerValue] == 200) {
             NSArray *arr = responseObject[@"data"][@"items"];
             if (arr.count > 0) {
+                [self.arrDatas removeAllObjects];
                 [self.arrDatas addObjectsFromArray:arr];
                 [self.tableView reloadData];
             }
-            
+            if ([responseObject[@"data"][@"isMore"] integerValue] == 0) {
+                [self.tableView.mj_footer endRefreshingWithNoMoreData];
+            } else {
+                
+            }
+            [self.tableView.mj_header endRefreshing];
         } else {
             [self.view makeToast:responseObject[@"enMsg"]];
         }
         
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         [MBProgressHUD hideHUDForView:self.view animated:YES];
+        [self.view makeToast:error.localizedDescription];
+    }];
+}
+
+- (void)loadMoreData {
+    _page += 1;
+    
+    NSString *url = [NSString stringWithFormat:@"http://94.191.30.13/notification/list?pageSize=10&pageNum=%ld", (long)_page];
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+
+    [manager GET:url parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+
+        if ([responseObject[@"code"] integerValue] == 200) {
+            NSArray *arr = responseObject[@"data"][@"items"];
+            if (arr.count > 0) {
+                [self.arrDatas addObjectsFromArray:arr];
+                [self.tableView reloadData];
+            }
+        } else {
+            [self.view makeToast:responseObject[@"enMsg"]];
+        }
+        
+        if ([responseObject[@"data"][@"isMore"] integerValue] == 0) {
+            [self.tableView.mj_footer endRefreshingWithNoMoreData];
+        } else {
+            [self.tableView.mj_footer endRefreshing];
+
+        }
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [self.tableView.mj_footer endRefreshing];
+
         [self.view makeToast:error.localizedDescription];
     }];
 }
